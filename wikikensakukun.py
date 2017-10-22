7f30dabf392a8c75916380035ef125ba8c2a621a
Shouta Takagi <shouta@takagishouta-no-MacBook-Pro.local>
2017-10-22 20:59:24 +0900

    wikiで柔道家と検索するプログラム

--- wikikensakukun.py
+++ wikikensakukun.py
@@ -13,30 +13,41 @@ a = 0
 maenoid = 0
 while a < 5:
 	params = {"count" : 1}
-	twitter = OAuth1Session(CK, CS, AT, AS)#OAuth認証
-	req = twitter.get("https://api.twitter.com/1.1/statuses/mentions_timeline.json", params = params)#最新のメンションを取得
-	mention = json.loads(req.text)#json形式からtext形式に
 
-	for tweet in mention:#getしたmentionからidとmention本文を抽出
+	twitter = OAuth1Session(CK, CS, AT, AS)
+	req = twitter.get("https://api.twitter.com/1.1/statuses/mentions_timeline.json", params = params) #最新のメンションを取得
+	tweetjouhou = json.loads(req.text) #json形式からtext形式に
+
+	for tweet in tweetjouhou:
 		id = tweet["id"]
-		men = tweet["text"]
-	mention_list = men.split(" ")#ユーザー名と本文を分割
+		mention = tweet["text"]
+		username = tweet["user"]["screen_name"]
+	mention_list = mention.split(" ") #ユーザー名と本文を分割
 	
-	if maenoid == id:#最新mentionのidと1つ前に取得したmentionのidをくらべてる
-		pass
+	if maenoid == id:
 		print("passしたよー")
+		pass
 	else:
 		print(id)
-		print(men)
-		wikipedia.set_lang("jp")#言語の設定
-		try:
-			wiki_serch = wikipedia.summary(mention_list[1] , sentences = 1)#wikipediaでmentionを検索し、検索結果の1文目を保存
-			print(wiki_serch)
-			params = {"status" : mention_list[0] + " " + wiki_serch}
-		except:
-			params = {"status" : mention_list[0] + " " + "該当なしだよ〜"} 
-		req = twitter.post("https://api.twitter.com/1.1/statuses/update.json", params = params)#検索結果をリプライする
-		maenoid = id
+		print(mention)
+		
+		men = [x for x in mention_list[1]]
+		if men[-2:] == [u'\u3068',u'\u306f']:#メンションが_とは_で終わっていたら
+			kensaku = [x for x in men[-2:]]
+			kensaku = ",".join(kensaku)
+			wikipedia.set_lang("jp")
+			try:
+				wiki_serch = wikipedia.summary(kensaku , sentences = 1)
+				print(wiki_serch)
+				params = {"status" : "@"+ username + " " + wiki_serch}
+			except:
+				params = {"status" : "@" + username + " " + "該当なしだよ〜"} 
+			req = twitter.post("https://api.twitter.com/1.1/statuses/update.json", params = params)
+			maenoid = id
+		else:
+			print("とはでおわってなかったよー")
+			mentionid = id
+			pass
 	
 	a = a + 1
 	time.sleep(100)
